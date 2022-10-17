45d71c144b5fae2fa039148d8af78767e7eb9ab0
deanthedream80 <dunningdean@gmail.com>
2022-10-17 01:16:46 -0500

    added font awesome

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 // import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-// import "font-awesome/css/font-awesome.css"
+import "font-awesome/css/font-awesome.css"
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
